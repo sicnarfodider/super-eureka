300a17f0636838db530241f60f4bb6cbec8e92db
Francis Redido <sicnarfodider@hotmail.com>
2018-01-19 12:39:09 -0800

    redux setup complete

---
+++ src/reducers/user-reducer.js
@@ -0,0 +1,17 @@
+import types from '../actions';
+
+
+const DEFAULT_STATE ={
+    auth: false
+}
+
+
+export default (state=DEFAULT_STATE, action)=>{
+    switch (action.type) {
+        // case expression:
+        //
+        //     break;
+        default:
+            return  state;
+    }
+}
