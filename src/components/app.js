dc557132c5b3ba1f1347bccf68bc826eb08288dc
Francis Redido <sicnarfodider@hotmail.com>
2018-01-19 12:14:34 -0800

    initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,11 @@
+import 'materialize-css/dist/css/materialize.min.css';
+
 import React from 'react';
-import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+
 
 const App = () => (
     <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
+        <h1 className="center-align">Change Later</h1>
     </div>
 );
 
